b4558464316cf7ff8a9b1f524950a87d96ec7fdd
Alex Horton <aihorton@wpi.edu>
2017-12-22 14:48:28 -0500

    Start of Loader file. It will be in charge of loading FBX files. Also added gitignore

--- Loader.js
+++ Loader.js
@@ -1,2 +1,31 @@
 var fbxloader = new THREE.FBXLoader();
-console.log(loader);
+
+/**
+ * Load Static FBX asset and add it to the scene
+ * @param fileName String name of the file to be loaded
+ * @param onLoad function to be executed on file load
+ * @param onProgress function to be executed on load progress
+ * @param onError function to be executed on error
+ */
+function loadStaticFBX(fileName, onLoad, onProgress, onError){
+    console.log("something");
+}
+
+/**
+ * Load animated FBX asset and add it to the scene
+ * @param fileName String name of the file to be loaded
+ * @param onLoad function to be executed on file load
+ * @param onProgress function to be executed on load progress
+ * @param onError function to be executed on error
+ * @param object optional object to save the asset to
+ */
+function loadAnimationFBX(fileName, onLoad, onProgress, onError, object){}
+
+/**
+ * Load Fbx file that contains the world
+ * @param fileName String name of the file to be loaded
+ * @param onLoad function to be executed on file load
+ * @param onProgress function to be executed on load progress
+ * @param onError function to be executed on error
+ */
+function loadWorldFBX(fileName, onLoad, onProgress, onError){}
