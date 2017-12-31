88377b4ae623834d3bbefbb1f742f26b3362915a
Tommy Trieu <trieutommy9@gmail.com>
2017-12-31 17:50:48 -0500

    Created initial methods for Logger class.

--- Logger.js
+++ Logger.js
@@ -7,7 +7,7 @@
  *    levelId: int representing level,
  *    date: date player played level,
  *    startTime: time player started level,
- *    levelDuration: time in seconds player took to complete level,
+ *    levelDuration: time in milliseconds player took to complete level,
  *    events: [
  *      {
  *        type: e.g. mouseclick, mouseover, etc.,
@@ -22,7 +22,7 @@
  *    tasks: [
  *      {
  *        name: name of task,
- *        duration: time in seconds relative to the start of the level when task was completed,
+ *        duration: time in milliseconds relative to the start of the level when task was completed,
  *        gradingCriteria: how to measure how well the user did on this task,
  *        grade: float indicating the amount of success achieved for this task by the user
  *      },
@@ -31,8 +31,78 @@
  *  }
  */
 class Logger {
-  constructor(playerId, levelId, date, startTime, levelDuration) {
+  /**
+   * Creates initial log.
+   * @param {String} playerId 
+   * @param {Number} levelId 
+   * @param {*} date 
+   * @param {Number} startTime 
+   */
+  constructor(playerId, levelId, startTime) {
+    this.log = {
+      playerId: playerId,
+      levelId: levelId,
+      date: date,
+      startTime: startTime,
+      levelDuration: 0.0,
+      events: [],
+      tasks: []
+    };
+    this.taskStartTime = 0;
+  }
 
+  /**
+   * Adds event to log.
+   * @param {String} type 
+   * @param {Number} time 
+   * @param {Number} x 
+   * @param {Number} y 
+   */
+  logEvent(type, time, x, y) {
+    this.log.events.push(
+      {
+        type: type,
+        time: time,
+        mouseCoordinate: {
+          x: x,
+          y: y
+        }
+      });
+  }
+
+  /**
+   * Records the start time of the current task.
+   * @param {Number} taskStartTime 
+   */
+  recordTaskStartTime(taskStartTime) {
+    this.taskStartTime = taskStartTime;
+  }
+
+  /**
+   * Adds task to log.
+   * @param {String} name 
+   * @param {Milliseconds} duration 
+   * @param {String} gradingCriteria 
+   * @param {Number} grade 
+   */
+  logTask(name, taskEndTime, gradingCriteria, grade) {
+    this.log.tasks.push(
+      {
+        name: name,
+        duration: taskEndTime - this.taskStartTime,
+        gradingCriteria: gradingCriteria,
+        grade: grade
+      });
+  }
+
+  /**
+   * Calculates the level duration and uploads log to DB.
+   * @param {Number} endTime 
+   */
+  endLog(endTime) {
+    this.log.levelDuration = endTime - this.log.startTime;
+
+    // log is send to DB
   }
 }
 
