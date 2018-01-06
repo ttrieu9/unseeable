8e7823bd6dee515c458b5f4ef79ec156482de59b
Tommy Trieu <trieutommy9@gmail.com>
2018-01-06 16:39:52 -0500

    Set up a mongoDB connection through mongoose.

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var mongoose = require('mongoose');
+var mongoUrl = 'mongodb://ttrieu:unseeable@ds239587.mlab.com:39587/unseeable-logger-test'
+
+mongoose.connect(mongoUrl, () => {
+  console.log('Connected to MongoDB.')
+});
+
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'MongoDB connection error.'));
 
 app.use(express.static(__dirname));
 
