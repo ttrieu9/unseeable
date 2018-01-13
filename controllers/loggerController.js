05f7c896447d2136ea2b56323bb45c42f0eafb45
Tommy Trieu <trieutommy9@gmail.com>
2018-01-13 16:11:07 -0500

    Added loggerController, only handles retrieving all lists from database and posting a log to the database. Both functions are unimplemented.

---
+++ controllers/loggerController.js
@@ -0,0 +1,11 @@
+var Log = require('../models/log');
+
+// Display all logs in database
+exports.read_logs = (req, res) => {
+  res.send('NOT IMPLEMENTED: create_log')
+};
+
+// Create a log
+exports.create_log = (req, res) => {
+  res.send('NOT IMPLEMENTED: create_log')
+};
