3d0d7318de9670571a5b98fddc36daea5c69e04d
Tommy Trieu <trieutommy9@gmail.com>
2018-01-13 16:05:37 -0500

    Added comments to server.js

--- server.js
+++ server.js
@@ -4,14 +4,19 @@ var path = require('path');
 var mongoose = require('mongoose');
 var mongoUrl = 'mongodb://ttrieu:unseeable@ds239587.mlab.com:39587/unseeable-logger-test'
 
+// Set up mongo connection
 mongoose.connect(mongoUrl, () => {
   console.log('Connected to MongoDB.')
 });
 
+// Get mongoose to use global promise library
 mongoose.Promise = global.Promise;
+
+// Get the connection
 var db = mongoose.connection;
 
-db.on('error', console.error.bind(console, 'MongoDB connection error.'));
+// Print error if one occurs
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 app.use(express.static(__dirname));
 
