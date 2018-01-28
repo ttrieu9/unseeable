fe1e4a4bbb3124cae65ef7492f86b6b2689f9c14
Tommy Trieu <trieutommy9@gmail.com>
2018-01-28 17:12:28 -0500

    Added some print statements for testing.

--- controllers/loggerController.js
+++ controllers/loggerController.js
@@ -2,10 +2,12 @@ var Log = require('../models/log');
 
 // Display all logs in database
 exports.read_logs = (req, res) => {
+  console.log('Read all existing logs.');
   res.send('NOT IMPLEMENTED: create_log')
 };
 
 // Create a log
 exports.create_log = (req, res) => {
+  console.log('Make a log.');
   res.send('NOT IMPLEMENTED: create_log')
 };
