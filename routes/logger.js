130f39722519cbf13bec667442089b7421e359b4
Tommy Trieu <trieutommy9@gmail.com>
2018-01-28 17:12:37 -0500

    Updated logger routes

--- routes/logger.js
+++ routes/logger.js
@@ -7,9 +7,9 @@ var logger_controller = require('../controllers/loggerController');
 // Logger routes
 
 // GET all existing logs
-router.get('/logger', logger_controller.read_logs);
+router.get('/', logger_controller.read_logs);
 
 // POST log to database
-router.post('/logger/create', logger_controller.create_log);
+router.post('/create', logger_controller.create_log);
 
 module.exports = router;
