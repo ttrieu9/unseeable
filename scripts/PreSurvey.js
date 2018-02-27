aca660c6aca0f77d8204dd5cac5715870d650335
Tommy Trieu <trieutommy9@gmail.com>
2018-02-27 07:34:04 -0500

    User preSurvey answers are successfully recorded, need to retrieve user ID and send data to DB.

--- scripts/PreSurvey.js
+++ scripts/PreSurvey.js
@@ -1,3 +1,92 @@
+init()
+
+/**
+ * Create event listeners.
+ */
+function init() {
+  var submit = document.getElementById('survey_submit_button');
+  submit.addEventListener('click', submitForm)
+
+  var selector = document.getElementById('colorblindness_selector');
+  selector.addEventListener('change', detectSelectorChange);
+}
+
+/**
+ * Reveals input box if user selects "Other..." in dropdown menu.
+ */
+function detectSelectorChange() {
+  var selector = document.getElementById('colorblindness_selector');
+  if(selector.options[selector.selectedIndex].value != 'Other...') {
+    document.getElementById('q1.1_input').style.display = 'none'
+  }
+  else {
+    document.getElementById('q1.1_input').style.display = 'inline'
+  }
+}
+
+/**
+ * Sends data to DB.
+ */
+function submitForm() {
+  let answers = recordAnswers();
+  sendPreSurvey(answers)
+}
+
+function recordAnswers() {
+  let answers = {
+    q1: '',
+    q1_1: '',
+    q1_2: '',
+    q2: ''
+  }
+
+  let q1 = document.getElementsByName('preSurvey_q1');
+  if(q1[0].checked) {
+    answers.q1 = q1[0].value;
+    
+    let selector = document.getElementById('colorblindness_selector');
+    if(selector.options[selector.selectedIndex].value != 'Other...') {
+      answers.q1_1 = selector.options[selector.selectedIndex].value
+    }
+    else {
+      let other = document.getElementById('q1.1_input');
+      answers.q1_1 = other.value;
+    }
+  }
+  else if(q1[1].checked) {
+    answers.q1 = q1[1].value;
+
+    let q1_2 = document.getElementsByName('preSurvey_q1.2');
+    for(var i = 0; i < q1_2.length; i++) {
+      if(q1_2[i].checked) {
+        answers.q1_2 = q1_2[i].value;
+      }
+    }
+  }
+
+  let q2 = document.getElementsByName('preSurvey_q2');
+  for(var i = 0; i < q2.length; i++) {
+    if(q2[i].checked) {
+      answers.q2 = q2[i].value;
+    }
+  }
+
+  return answers;
+}
+
+function sendPreSurvey(answers) {
+  let results = {
+    userId: '',
+    q1: answers.q1,
+    q1_1: answers.q1_1,
+    q1_2: answers.q1_2,
+    q2: answers.q2
+  }
+
+  // TODO: AJAX POST Request
+  console.log(results)
+}
+
 /**
  * Reveals drop down list of colorblindnesses.
  */
