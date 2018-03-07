298cfc25e0e1fa6e372f9a652a08657631e0dcaa
Tommy Trieu <trieutommy9@gmail.com>
2018-03-06 22:05:21 -0500

    Implemented feature where unique user ID is generated for a session. Only works if user starts on a specific page.

--- routes/userId.js
+++ routes/userId.js
@@ -7,6 +7,8 @@ var user_id_controller = require('../controllers/userIdController');
 // Logger routes
 
 // GET user ID
-router.get('/', user_id_controller.generate_user_id);
+router.get('/generate', user_id_controller.generate_user_id);
+
+router.get('/retrieve', user_id_controller.get_user_id);
 
 module.exports = router;
